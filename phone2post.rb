3c838967577743455e255bed1634d9a382e34e36
Jamie Lawrence <jamie@shutterscouts.com>
2010-04-15 00:05:46 +0100

    debugging

--- phone2post.rb
+++ phone2post.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'sinatra'
+
  
 set :users, {'1234' => 'hopeless'} 
 
@@ -9,6 +10,6 @@ end
 
 post '/upload/:apikey' do
 "Hello #{params[:apikey]} #{settings.users['1234']}"
-puts params
-params[:file][:filename]
+puts params.to_yaml
+params[:filename][:filename]
 end

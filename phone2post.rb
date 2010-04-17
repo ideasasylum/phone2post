9f4232708819f86beef1c6412c3334fdbd766ca0
Jamie Lawrence <jamie@shutterscouts.com>
2010-04-17 23:26:48 +0100

    extracted posterous config into a YAML file

--- phone2post.rb
+++ phone2post.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 require 'sinatra'
 require 'curb' 
 
-set :users, {'1234' => {'username' => 'hopeless', 'password' => 'Claire.'} }
+set :users, YAML::parse_file('posterous.yaml')
 
 get '/' do
     haml :index
@@ -13,15 +13,19 @@ post '/upload/4367803575802/:apikey' do
     user = settings.users[apikey]
     # receive mp3 recording from Tropo
     puts params.to_yaml
-    filepath = params[:filename][:tempfile].path
+    file = params[:filename][:tempfile]
 
+    # rename the file
+    date = Date.today.to_s
+    new_file = File.join(File.dirname(file), ,File.extname(file))
+    File.rename file.path, new_file 
     
     # upload to posterous
     posterous_params = {'title' => 'my test post', 
                 'autopost' => '1', 'source' => 'phone2post', 'sourceLink' => 'http://phone2post.heroku.com',
                 'tags' => 'podcast'}.merge user 
     post_data = posterous_params.map {|k, v| Curl::PostField.content(k, v)} 
-    post_data << Curl::PostField.file('media', filepath) 
+    post_data << Curl::PostField.file('media', new_file) 
     c = Curl::Easy.new('http://posterous.com/api/upload') 
     c.multipart_form_post = true 
     c.http_post(*post_data) 

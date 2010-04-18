5ca3719b34d1a6c267c915fc0c891f52b86d72b8
Jamie Lawrence <jamie@shutterscouts.com>
2010-04-18 10:53:54 +0100

    fixed file cast error

--- phone2post.rb
+++ phone2post.rb
@@ -10,14 +10,14 @@ get '/' do
 end
 
 post '/upload/4367803575802/:apikey' do
+    # Get user's posterous details
     apikey = params[:apikey]
     user = settings.users[apikey]
+    
     # receive mp3 recording from Tropo
-    puts params.to_yaml
-
     file = params[:filename][:tempfile] 
-    filepath = file.path if file.is_a? File
-    filepath ||= File.new(file).path  
+    filepath = File.new(file).path if file.is_a? String  
+    filepath ||= file.path
 
     # rename the file
     date = Date.today.to_s
@@ -25,8 +25,9 @@ post '/upload/4367803575802/:apikey' do
     File.rename file, new_file 
     
     # upload to posterous
-    debugger
-    posterous_params = user.merge({'autopost' => '1', 'source' => 'phone2post', 'sourceLink' => 'http://phone2post.heroku.com', 'tags' => 'podcast'})
+    posterous_params = user.merge({'autopost' => '1', 'source' => 'phone2post', 
+                                   'sourceLink' => 'http://phone2post.heroku.com', 
+                                   'tags' => 'podcast'})
     post_data = posterous_params.map {|k, v| Curl::PostField.content(k, v)} 
     post_data << Curl::PostField.file('media', new_file) 
     c = Curl::Easy.new('http://posterous.com/api/upload') 
